429929209c57b732f4636f02cfa572411c0418f2
esalami19 <esalami19@yahoo.co.uk>
2024-07-02 00:14:26 +0100

    node modules

---
+++ global.js
@@ -0,0 +1,11 @@
+import { log } from "./logger";
+
+// this ./ to signify current folder 
+// this ../ parent folder 
+
+function greeting (name){
+return `hiya ${name}, how you doing?`
+}
+
+console.log(greeting('lii'))
+console.log(log)
---
+++ logger.js
@@ -0,0 +1,11 @@
+let url = "http://mylogger.io/log"
+
+export function log (message){
+    console.log(message)
+}
+
+
+//for common module I believe
+//module.exports.log = log
+//internally url exported end point
+//module.exports.endPoint = url
