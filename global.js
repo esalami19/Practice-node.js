fb1c49bd0713611e0335b499126c4e0950fed7c2
esalami19 <esalami19@yahoo.co.uk>
2024-07-02 15:05:08 +0100

    operating system storage method

--- global.js
+++ global.js
@@ -1,9 +1,18 @@
 import { log } from "./modules/logger.js";
 import path from "path"
+import os from "os"
 
 const fileName = "C:\Users\L. Salami\Documents\Elizabeth\Practice\Practice-nodejs\modules\logger.js"
 const pathName = path.parse(fileName)
 console.log(pathName)
+
+const osTMemory = os.totalmem
+const osFMemory = os.freemem
+console.log(
+    `Free memory is: ${osFMemory}
+     Total memory is: ${osTMemory}
+    `)
+
 // this ./ to signify current folder 
 // this ../ parent folder 
 
