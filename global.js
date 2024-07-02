c349990e62e831defec7c90f4b91cf77ba762ab4
esalami19 <esalami19@yahoo.co.uk>
2024-07-02 14:38:19 +0100

    parse method

--- global.js
+++ global.js
@@ -1,5 +1,9 @@
 import { log } from "./modules/logger.js";
+import path from "path"
 
+const fileName = "C:\Users\L. Salami\Documents\Elizabeth\Practice\Practice-nodejs\modules\logger.js"
+const pathName = path.parse(fileName)
+console.log(pathName)
 // this ./ to signify current folder 
 // this ../ parent folder 
 
@@ -8,7 +12,7 @@ return `hiya ${name}, how you doing?`
 }
 
 console.log(greeting('lii'))
-log("hiya")
+console.log(log)
 
 
 //npm init -y 
